861b84ec23497d8f5fd5b9bb6e259cd1c80bcf45
Umair Mustafa <umairmustafa753@gmail.com>
2021-02-21 18:20:30 +0500

    update expo version and remove bugs

--- Screens/home.js
+++ Screens/home.js
@@ -1,153 +1,187 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  Text,
+  Alert
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { Audio } from "expo-av";
-import * as DocumentPicker from 'expo-document-picker';
-import LinkedList from '../linkList';
-
+import * as DocumentPicker from "expo-document-picker";
+import LinkedList from "../linkList";
 
 export default function HomeScreen() {
+  const DEFAULTTEXT = "Oops! no Song in List";
+
   const [songs, setSongs] = useState(new LinkedList());
   const [currentSongIndex, setCurrentSongIndex] = React.useState(0);
+  const [songName, setSongName] = React.useState(DEFAULTTEXT);
   const [isPlaying, setIsPlaying] = useState(false);
   let index = currentSongIndex;
-  
-  
+
   const didMount = async () => {
     console.disableYellowBox = true;
     try {
       await Audio.setAudioModeAsync({
-       allowsRecordingIOS: false,
-       interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-       playsInSilentModeIOS: true,
-       interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
-       shouldDuckAndroid: true,
-       staysActiveInBackground: true,
-       playThroughEarpieceAndroid: true,
-      })
-      this.loadAudio()
-     } catch (e) {
-      console.log(e)
-     }
-  }
-  
+        allowsRecordingIOS: false,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+        shouldDuckAndroid: true,
+        staysActiveInBackground: true,
+        playThroughEarpieceAndroid: true
+      });
+      this.loadAudio();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
-    didMount();    
+    didMount();
     return () => {
       pauseSong();
-    }
-  }, [])
+    };
+  }, []);
 
-  const gotoPickSongFunc = async () => { 
+  const gotoPickSongFunc = async () => {
     pauseSong();
     pickSong();
-  }
+  };
 
   const pickSong = async () => {
     let result = await DocumentPicker.getDocumentAsync({});
-    songs.add(result);
-    loadSongfunc();
-  }
+    console.log({ result });
+    if (result.name) {
+      if (!songs.checkNode(result.name)) {
+        songs.add(result);
+        Alert.alert(`${result.name} added successfully.`);
+        loadSongfunc();
+      } else {
+        Alert.alert(`${result.name} already added.`);
+      }
+    }
+  };
 
   const loadSongfunc = async () => {
     let NowPlaying = songs.elementAt(index);
-    alert( 'Now Playing ' + NowPlaying.name );
+    setSongName(NowPlaying.name);
     soundObject = new Audio.Sound();
-    await soundObject.loadAsync({uri: NowPlaying.uri});
-  }
+    await soundObject.loadAsync({ uri: NowPlaying.uri });
+  };
 
   const btnPlayAndPauseClicked = async () => {
     let NowPlaying = songs.elementAt(index);
-    if( NowPlaying != null ){
-      isPlaying == false  ? playSong() : pauseSong() ;
-      isPlaying == false  ? setIsPlaying(true) : setIsPlaying(false);
+    if (NowPlaying != null) {
+      isPlaying == false ? playSong() : pauseSong();
+      isPlaying == false ? setIsPlaying(true) : setIsPlaying(false);
     }
-  }
+  };
 
   const playSong = async () => {
-    await soundObject.playAsync()
-  }
+    await soundObject.playAsync();
+  };
 
   const pauseSong = async () => {
-    await soundObject.pauseAsync()
-  }
+    await soundObject.pauseAsync();
+  };
   const stopSong = async () => {
     await soundObject.stopAsync();
-  }
+  };
 
   const btnDeleteSong = () => {
     songs.removeAt(index);
-    if( 0 < index ){
+    setIsPlaying(false);
+    if (0 < index) {
       index = songs.size() - 1;
-      setCurrentSongIndex( index );
+      setCurrentSongIndex(index);
       stopSong();
       loadSongfunc();
+    } else if (0 == index) {
+      stopSong();
+      loadSongfunc();
+    }
+    if (songs.size() == 0) {
+      Alert.alert("you have on song left");
+      setSongName(DEFAULTTEXT);
+      loadSongfunc();
+      stopSong();
+    }
+  };
 
-    }
-    else if( 0 == index ){
-      stopSong();
-      loadSongfunc();
-
-    }
-    else if(songs.size() == 0){
-      alert('you have on song left')
-      loadSongfunc();
-      stopSong();
-    }
-  }
-  
   const btnNextSong = () => {
+    setIsPlaying(false);
     ++index;
-    setCurrentSongIndex( index );
-    if( songs.size() >  index ){
+    setCurrentSongIndex(index);
+    if (songs.size() > index) {
       stopSong();
       loadSongfunc();
-    }
-    else{
+    } else {
       --index;
-      setCurrentSongIndex( index );
-      alert('No next song avaliable');
+      setCurrentSongIndex(index);
+      Alert.alert("No next song avaliable");
     }
-  }
-
+  };
 
   const btnPrevSong = () => {
+    setIsPlaying(false);
     --index;
-    setCurrentSongIndex( index );
-    if( 0 <= index ){
-    stopSong();
-    loadSongfunc();
-    }
-    else{
+    setCurrentSongIndex(index);
+    if (0 <= index) {
+      stopSong();
+      loadSongfunc();
+    } else {
       ++index;
-      setCurrentSongIndex( index );
-      alert('No next song avaliable');
+      setCurrentSongIndex(index);
+      Alert.alert("No previous song avaliable");
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Ionicons name='ios-headset' size={300} color='#1A535C' />
+      <Ionicons name="ios-headset" size={300} color="#1A535C" />
+      <Text style={styles.songNameText}>
+        {songName.length > 25 ? songName.substring(0, 25) + "..." : songName}
+      </Text>
       <View style={styles.iconContainer}>
-      <TouchableOpacity style={styles.control} onPress = { gotoPickSongFunc } >
-        <Ionicons name='ios-filing' size={48} color='#444' />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.control} >
-        <Ionicons name='ios-skip-backward' size={48} color='#444' onPress={ btnPrevSong } />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.control} onPress = { btnPlayAndPauseClicked }>
-        {isPlaying == false  ?
-        <Ionicons name='ios-play-circle' size={88} color='#444' />
-        :
-        <Ionicons name='md-pause' size={88} color='#444' />
-        } 
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.control} >
-        <Ionicons name='ios-skip-forward' size={48} color='#444' onPress={ btnNextSong }/>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.control} >
-        <Ionicons name='ios-close-circle' size={48} color='#444' onPress={ btnDeleteSong }/>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.control} onPress={gotoPickSongFunc}>
+          <Ionicons name="ios-filing" size={48} color="#444" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.control}>
+          <Ionicons
+            name="ios-skip-backward"
+            size={48}
+            color="#444"
+            onPress={btnPrevSong}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.control}
+          onPress={btnPlayAndPauseClicked}
+        >
+          {isPlaying == false ? (
+            <Ionicons name="ios-play-circle" size={88} color="#444" />
+          ) : (
+            <Ionicons name="md-pause" size={88} color="#444" />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.control}>
+          <Ionicons
+            name="ios-skip-forward"
+            size={48}
+            color="#444"
+            onPress={btnNextSong}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.control}>
+          <Ionicons
+            name="ios-close-circle"
+            size={48}
+            color="#444"
+            onPress={btnDeleteSong}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -156,25 +190,29 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   iconContainer: {
     flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    maxWidth: 130
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   control: {
-		margin: 20
-	},
+    margin: 20
+  },
   containerText: {
-    color: '#1A535C',
+    color: "#1A535C",
     fontSize: 50,
-    margin: 20,
+    margin: 20
+  },
+  songNameText: {
+    fontSize: 20
   },
   getStartedButtonText: {
-    color: 'white',
+    color: "white"
   }
 });
--- linkList.js
+++ linkList.js
@@ -1,65 +1,86 @@
-function LinkedList() { 
-    var length = 0; 
-    var head = null; 
-  
-    var Node = function(song){
-      this.song = song; 
-      this.next = null; 
-    }; 
-  
-    this.size = function(){
-      return length;
-    };
-  
-    this.head = function(){
-      return head;
-    };
-  
-    this.add = function(song){
-      var node = new Node(song);
-      if(head === null){
-          head = node;
-      } else {
-          var currentNode = head;
-  
-          while(currentNode.next){
-              currentNode  = currentNode.next;
-          }
-          currentNode.next = node;
-      }
-      length++;
-    }; 
-  
-    this.elementAt = function(index) {
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function (song) {
+    this.song = song;
+    this.next = null;
+  };
+
+  this.size = function () {
+    return length;
+  };
+
+  this.head = function () {
+    return head;
+  };
+
+  this.add = function (song) {
+    var node = new Node(song);
+    if (head === null) {
+      head = node;
+    } else {
       var currentNode = head;
-      var count = 0;
-      while (count < index){
-          count ++;
-          currentNode = currentNode.next
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
       }
-      return currentNode.song;
-    };    
-    
-    this.removeAt = function(index) {
-      var currentNode = head;
-      var previousNode;
-      var currentIndex = 0;
-      if (index < 0 || index >= length){
-          return null
-      }
-      if(index === 0){
-          head = currentNode.next;
-      } else {
-          while(currentIndex < index) {
-              currentIndex ++;
-              previousNode = currentNode;
-              currentNode = currentNode.next;
-          }
-          previousNode.next = currentNode.next
-      }
-      length--;
-      return currentNode.song;
+      currentNode.next = node;
     }
-  } 
+    length++;
+  };
+
+  this.elementAt = function (index) {
+    var currentNode = head;
+    var count = 0;
+    while (count < index) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return currentNode.song;
+  };
+
+  this.checkNode = function (name) {
+    var currentNode = head;
+    var nodeFound = false;
+    if (head == null) {
+      return nodeFound;
+    } else {
+      var currentNode = head;
+      if (currentNode.song.name === name) {
+        nodeFound = true;
+      }
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+        if (currentNode.song.name === name) {
+          nodeFound = true;
+        }
+      }
+    }
+    return nodeFound;
+  };
+
+  this.removeAt = function (index) {
+    var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+    if (index < 0 || index >= length) {
+      //exception
+      return null;
+    }
+    if (index === 0) {
+      head = currentNode.next;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode; // 2
+        currentNode = currentNode.next; // 3
+      }
+      previousNode.next = currentNode.next; //  3 || 4
+    }
+    length--;
+    return currentNode.song;
+  };
+}
 
 export default LinkedList;
